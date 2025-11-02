4f5d3981c22ce9599e84ec4bab57af6b11b0049d
Artem Terenin <artemterenin@gmail.com>
2025-11-02 22:05:44 +0300

    feat: add tests directory and create pagesData.js for test data

---
+++ tests/data/pagesData.js
@@ -0,0 +1,16 @@
+const pagesData = [
+  {
+    name: 'Home',
+    url: '/index.html',
+  },
+  {
+    name: 'About',
+    url: '/about.html',
+  },
+  {
+    name: 'Contact',
+    url: '/contact.html',
+  },
+];
+
+export default pagesData;
